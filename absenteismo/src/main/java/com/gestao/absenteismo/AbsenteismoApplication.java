8b62f312180239c8b95c3865a293e319f6fd5273
yujiichikawa <thiagogelinski20@gmail.com>
2023-09-09 16:56:40 -0300

    criação do projeto

---
+++ absenteismo/src/main/java/com/gestao/absenteismo/AbsenteismoApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.absenteismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenteismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenteismoApplication.class, args);
+	}
+
+}
---
+++ absenteismo/src/test/java/com/gestao/absenteismo/AbsenteismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.absenteismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsenteismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
