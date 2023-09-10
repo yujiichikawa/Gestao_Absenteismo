4452e7163c53653edf83b0d74d690257e66d02aa
yujiichikawa <thiagogelinski20@gmail.com>
2023-09-10 19:14:14 -0300

    Substituindo o mysql pelo banco h2

---
+++ absenteismo/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO contatos(email,telefone) VALUE("Thiagoyuji@gmail.com","88888888888");
+INSERT INTO contatos(email,telefone) VALUE("catia@gmail.com","85522335674");
+INSERT INTO contatos(email,telefone) VALUE("roberto@gmail.com","74445525478");
+INSERT INTO contatos(email,telefone) VALUE("francisco@gmail.com","26987535261");
+INSERT INTO funcionarios(atuacao,cargo,cpf,nome,id_contatos) VALUE("PINTURA","COLABORADOR","11111111111","Thiago Yuji",1);
+INSERT INTO funcionarios(atuacao,cargo,cpf,nome,id_contatos) VALUE("PINTURA","GERENTE_DE_EQUIPE","22222222222","Catia Lais",2);
+INSERT INTO funcionarios(atuacao,cargo,cpf,nome,id_contatos) VALUE("ARTIFICE","COLABORADOR","33333333333","Roberto Carvalho",3);
+INSERT INTO funcionarios(atuacao,cargo,cpf,nome,id_contatos) VALUE("ARTIFICE","GERENTE_DE_EQUIPE","44444444444","Francisco albuquerque",4);
